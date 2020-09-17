c6f7f64a8b97579450c397448225b174cc47224d
Jayesh Tiwari <JayeshTiwari03@users.noreply.github.com>
2020-09-17 23:41:39 +0530

    header and footer built styles, carousel

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Carousel from './Carousel'
 
 const Featured = () => {
     return (
         <div style={{position: 'relative'}}>
+
+            <Carousel />
+
             <div className='artist_name'>
             <div className='wrapper'>
                 Juice WRLD
